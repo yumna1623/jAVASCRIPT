2bacf3a49f346a063def0dbda194a79f77a23f34
yumna1623 <yumnanasir1623@gmail.com>
2024-10-12 23:01:39 +0500

    prototype

--- app.js
+++ app.js
@@ -712,55 +712,202 @@
 
     // -------------------------------------or -------------------------------------
     // chaining
-    // const username=promiseFive
-    //         .then((user)=>{
-    //             console.log(user)
-    //             return user.username
-    //         }).then((username)=>{
-    //             console.log(username);
+//     const username=promiseFive
+//             .then((user)=>{
+//                 console.log(user)
+//                 return user.username
+//             }).then((username)=>{
+//                 console.log(username);
                 
-    //         }).catch((error)=>{
-    //             console.log(error);
+//             }).catch((error)=>{
+//                 console.log(error);
                 
-    //         }).finally(()=>{console.log("promise5 is either resolved or rejected");
-    //         })
+//             }).finally(()=>{console.log("promise5 is either resolved or rejected");
+//             })
 
 
-// --------------------try and catch format
-    // const getAllUser = async()=>{
-    //     try {
-    //         const response=await fetch('https://jsonplaceholder.typicode.com/user')
-    //         const data=await response.json()
-    //         console.log(data); 
-    //     } catch (error) {
-    //         console.log("error",error);
+// // --------------------try and catch format
+//     const getAllUser = async()=>{
+//         try {
+//             const response=await fetch('https://jsonplaceholder.typicode.com/user')
+//             const data=await response.json()
+//             console.log(data); 
+//         } catch (error) {
+//             console.log("error",error);
             
-    //     }  
-    // }
-    // getAllUser()
+//         }  
+//     }
+//     // getAllUser()
 
-    // fetch('https://jsonplaceholder.typicode.com/user')
-    // .then((Response)=>{
-    //     return Response.json()
-    // })
-    // .catch((data)=>{
-    //     console.log(data);
+//     fetch('https://jsonplaceholder.typicode.com/user')
+//     .then((response)=>{
+//         return response.json()
+//     })
+//     .then((data)=>{
+//         console.log(data);
+        
+//     })
+//     .catch((error)=>{
+//         console.log(error);
 
-    // })
+//     })
 
 
 // --------------------------------------------LEACTURE 41--------------------------------
 // FETCH
 
-
-
-
-
-
 // --------------------------------------------LEACTURE 42--------------------------------
+// OOP 
+
 // --------------------------------------------LEACTURE 43--------------------------------
+//prototype
+//gave the access of this and new keyword
+
+// in prototype we get access of some methods 
+// we can change methods in protoype of our own 
+
+
+// function multiplyBy5(num) {
+//      return num*5
+// }
+// multipleBy5.power = 2
+
+// console.log(multipleBy5(5));
+// console.log(multipleBy5.power);
+// console.log(multipleBy5.prototype); //empty{}
+
+// function createUser(username,score) {
+//     this.username=username 
+//     this.score=score
+// }
+
+// createUser.prototype.increment=function () {
+//     this.score++;    
+// }
+// createUser.prototype.printMe=function () {
+//     console.log(`score is ${this.score}`);
+// }
+
+// const chai = new createUser("chai",25)
+// const tea = new createUser("tea",250)
+
+// chai.printMe()
+
+// Here's what happens behind the scenes when the new keyword
+//  is used:
+
+// A new object is created: The new keyword initiates the 
+// creation of a new JavaScript object.
+
+// A prototype is linked: The newly created object gets linked to the prototype 
+// property of the constructor function. This means that it has
+//  access to properties and methods defined on the constructor's prototype.
+
+// The constructor is called: The constructor function is 
+// 
+// called with the specified arguments and this is bound to the newly created object. 
+// If no explicit return value is specified from the constructor, JavaScript assumes this, the newly created object,
+//  to be the intended return value.
+
+// The new object is returned: After the constructor function has been 
+// called, if it doesn't return a non-primitive value (object, array, function, etc.),
+//  the newly created object is returned.
+// ---------------------------------------
+
+// let myName = "hitesh     "
+// let mychannel = "chai     "
+
+// console.log(myName.trueLength);
+
+
+// let myHeros = ["thor", "spiderman"]
+
+
+// let heroPower = {
+//     thor: "hammer",
+//     spiderman: "sling",
+
+//     getSpiderPower: function(){
+//         console.log(`Spidy power is ${this.spiderman}`);
+//     }
+// }
+
+// Object.prototype.hitesh = function(){
+//     console.log(`hitesh is present in all objects`);
+// }
+
+// Array.prototype.heyHitesh = function(){
+//     console.log(`Hitesh says hello`);
+// }
+
+// heroPower.hitesh()
+// myHeros.hitesh()
+
+// myHeros.heyHitesh() //accessible cause this is an array
+// heroPower.heyHitesh() //not accessible cause this is an array
+
+// inheritance
+
+// const User = {
+//     name: "chai",
+//     email: "chai@google.com"
+// }
+
+// const Teacher = {
+//     makeVideo: true
+// }
+
+// const TeachingSupport = {
+//     isAvailable: false
+// }
+
+// const TASupport = {
+//     makeAssignment: 'JS assignment',
+//     fullTime: true,
+//     __proto__: TeachingSupport
+// }
+
+// Teacher.__proto__ = User
+
+// // modern syntax
+// Object.setPrototypeOf(TeachingSupport, Teacher)
+
+// let anotherUsername = "ChaiAurCode     "
+
+// String.prototype.trueLength = function(){
+//     console.log(`${this}`);
+//     console.log(`True length is: ${this.trim().length}`);
+// }
+
+// anotherUsername.trueLength()
+// "hitesh".trueLength()
+// "iceTea".trueLength()
+
+
 // --------------------------------------------LEACTURE 44--------------------------------
+// Call and this in javascript |
+
+// function SetUsername(username){
+//     //complex DB calls
+//     this.username = username
+//     console.log("called");
+// }
+
+// function createUser(username, email, password){
+//     SetUsername.call(this.username)
+   
+//     this.email = email
+//     this.password = password
+// }
+
+// const chai = new createUser("chai", "chai@fb.com", "123")
+// console.log(chai);
+
+
 // --------------------------------------------LEACTURE 45--------------------------------
+// Class constructor and static 
+
+
 // --------------------------------------------LEACTURE 46--------------------------------
 // --------------------------------------------LEACTURE 47--------------------------------
 // --------------------------------------------LEACTURE 48--------------------------------
