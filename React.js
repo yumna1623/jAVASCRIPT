198643c464708d289ae95f1e184af455d1220c0f
Yumna Nasir <166024894+yumna1623@users.noreply.github.com>
2024-12-10 21:46:21 +0500

    Update React.js
    state unstate hooks

--- React.js
+++ React.js
@@ -133,10 +133,26 @@ function PlayButton({children,onClick}) {
 }
 export default PlayButton;
 
-----------------------------------------------------------------------------------------------------------
-
-
-----------------------------------------------------------------------------------------------------------
+----------------------------------------------STATE UNSTATE / HOOKS----------------------------------------
+import { useState } from "react";
+function Counter() {
+    const [number,setNumber] = useState(0);
+    function handleClick(e) {
+        e.stopPropagation();
+        setTimeout(()=>{
+            setNumber(number+1); 
+        },2000)
+        console.log(number);
+    }
+    return(
+        <>
+        <h1 style={{color : 'white'}}>{number}</h1>
+        <button onClick={handleClick}>Add</button>
+        </>
+    )
+}
+export default Counter;
+---------------------------------------------------------------------------------------------------------
 
 ----------------------------------------------------------------------------------------------------------
 
